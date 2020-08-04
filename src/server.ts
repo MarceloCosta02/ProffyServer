e25df513a7f87ec06c02c5b07dc49cc33caafb59
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-08-04 20:35:31 -0300

    Configurando o banco de dados sqlite com knex para escrita de querys em js

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (req, res) => {
-    return res.json([
-       { name: "Marcelo", cor: "Azul" }
-    ])
+app.get('/', (req, res) => {
+    return res.json({ message: 'Hello World' });
 });
 
 app.listen(3333);
