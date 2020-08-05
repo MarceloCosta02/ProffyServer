98ab30b688d7e03bcd4be339acc06bffceed1acd
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-08-04 22:39:09 -0300

    Criando e configurando as migrations

---
+++ src/database/migrations/03_create_connections.ts
@@ -0,0 +1,22 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('connections', table => {
+        table.increments('id').primary();
+
+        table.integer('user_id')
+        .notNullable()
+        .references('id')
+        .inTable('user')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE'); // Caso o id do usuario seja deletado, ele deleta em cascata tudo oq contem a FK dele na tabela de classes
+    
+        table.timestamp('created_at')
+            .defaultTo('now()')
+            .notNullable();
+    });
+}
+
+export async function down(knex: Knex) {
+    knex.schema.dropTable('connections');
+}
